c0117cee10e4887abc1ae101c34007fb3a92b62c
EventorMoldova <andrei.golovei@gmail.com>
2025-01-28 15:25:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 from keep_alive import keep_alive
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackContext
 
 # Tokenul tău de bot Telegram (trebuie să-l înlocuiești cu cel real)
 TELEGRAM_TOKEN = "7849522428:AAFKfnqj3We0frTBpVZudMr6kS0rYU0fCso"
 
-# Funcția care răspunde la mesaje
-async def handle_message(update: Update, context: CallbackContext) -> None:
-    # Verifică dacă mesajul provine dintr-un chat privat
-    if update.message.chat.type == "private":
-        # Răspunde în chatul privat cu mesajul dorit
-        await update.message.reply_text('Eu nu folosesc Telegramul, dar pot să-ți răspund!')
-
 # Funcția pentru comanda /start
 async def start(update: Update, context: CallbackContext) -> None:
-    if update.message.chat.type == "private":
-        await update.message.reply_text('222Salut! Eu sunt un bot care nu folosește Telegram. Cum te pot ajuta?')
+    await update.message.reply_text('Salut! Eu sunt un bot care nu folosește Telegram. Cum te pot ajuta?')
 
 # Rularea botului
 def main():
@@ -25,9 +17,6 @@ def main():
     # Adaugă handler-ul pentru comanda /start
     application.add_handler(CommandHandler("start", start))
     
-    # Adaugă handler-ul pentru mesaje (verifică dacă este un mesaj privat)
-    application.add_handler(MessageHandler(filters.Filters.all, handle_message))  # Răspunde la orice mesaj
-
     # Începe botul
     application.run_polling()
 
