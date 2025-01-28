d0cbff8f4ddca007477b786a3f04521d3a844d0c
EventorMoldova <andrei.golovei@gmail.com>
2025-01-28 12:22:52 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler
+from keep_alive import keep_alive
+
+def start(update: Update, context):
+    update.message.reply_text("Salut! Sunt un bot activ!")
+
+def main():
+    updater = Updater("7849522428:AAFKfnqj3We0frTBpVZudMr6kS0rYU0fCso", use_context=True)
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+
+if __name__ == "__main__":
+    keep_alive()
+    main()
